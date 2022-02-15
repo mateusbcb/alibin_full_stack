a34b90d72e85e1f4ef9dab007921fb3c392972af
mateusbcb <mateusbcb@gmail.com>
2022-02-15 16:35:13 -0300

    Criação de migrations, routes, models e controllers de Cliente e loja

---
+++ database/migrations/2022_02_15_185242_create_lojas_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLojasTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lojas', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('cliente_id');
+            $table->string('nome');
+            $table->string('preco');
+            $table->string('codigo');
+            $table->timestamps();
+
+            $table->foreign('cliente_id')->references('cliente')->on('id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lojas');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ClienteController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -12,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+Route::resource('/cliente', ClienteController::class);
+Route::resource('/loja', ClienteController::class);
 
 Route::get('/', function () {
     return view('welcome');
